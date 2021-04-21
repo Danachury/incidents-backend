ca9494619351b1cc04266703f262c79b5b05cf63
Denilson Anachury <denilsoncastro1997@gmail.com>
2021-04-21 09:38:13 -0500

    Update re

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,7 +1,7 @@
 export class AppConfig {
   static PORT = process.env.PORT || '3000'
   static CONTEXT = process.env.CONTEXT || '/poc-ib'
-  static DEBUG = process.env.DEBUG || 'poc-incidents_backend:server'
+  static DEBUG = process.env.DEBUG || 'incidents-backend:server'
   static LOGGER_FORMAT = process.env.LOGGER_FORMAT || 'dev'
   static DB_USER = process.env.DB_USER || 'incuser'
   static DB_PASS = process.env.DB_PASS || '5MLlFPmeP5zoZ3VK'
